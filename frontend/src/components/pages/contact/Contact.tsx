3a0b25cc85cab29564f66138bcaf5c45b47e8354
HannaGregorowicz <33255939+HannaGregorowicz@users.noreply.github.com>
2020-11-15 12:13:52 +0100

    Make contact page responsive

--- frontend/src/components/pages/contact/Contact.tsx
+++ frontend/src/components/pages/contact/Contact.tsx
@@ -1,19 +1,29 @@
 import React from "react";
+import { useMediaQuery } from "react-responsive";
 import Map from "./Map";
 import Address from "./Address";
 
-const gridContainerStyle: React.CSSProperties = {
+const gridDesktopStyle: React.CSSProperties = {
   display: "grid",
   gridTemplateColumns: "1fr 1fr"
 };
 
+const gridSmallStyle: React.CSSProperties = {
+  display: "grid",
+  gridTemplateColumns: "1fr"
+};
+
 const Contact = () => {
+  const isMobile = useMediaQuery({
+    query: "(max-width: 600px)"
+  });
+
   return (
     <>
       <div className="bottomDivider"></div>
       <div className="contentContainer">
         <h2 className="center">Zapraszamy do kontaktu!</h2>
-        <div style={gridContainerStyle}>
+        <div style={isMobile ? gridSmallStyle : gridDesktopStyle}>
           <Map />
           <Address />
         </div>

8f705ce61ebb7ebb4a7f6b0808b4645e03727fd8
HannaGregorowicz <33255939+HannaGregorowicz@users.noreply.github.com>
2020-11-24 22:56:09 +0100

    Move and change config file

--- backend/src/config.ts
+++ backend/src/config.ts
@@ -2,7 +2,9 @@ import dotenv from "dotenv";
 dotenv.config();
 
 const config = {
-  DB_URL: process.env.DB_URL
+  DB_URL: process.env.DB_URL,
+
+  JWT_KEY: process.env.JWT_KEY
 };
 
 export default config;

22a8bc7f37986cfcbfe9e92c5c567ad30624d60d
HannaGregorowicz <33255939+HannaGregorowicz@users.noreply.github.com>
2020-12-06 02:42:53 +0100

    Fix deprecation warning

--- backend/src/database/connection.ts
+++ backend/src/database/connection.ts
@@ -4,6 +4,7 @@ import config from "../config";
 export const connectToDatabase = () => {
   mongoose.set("useNewUrlParser", true);
   mongoose.set("useUnifiedTopology", true);
+  mongoose.set("useFindAndModify", false);
 
   mongoose.connect(config.DB_URL, () => {
     console.log("Connected to database.");

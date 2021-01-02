c0c179766a0e983f02eeaa16e0794744811592b9
HannaGregorowicz <33255939+HannaGregorowicz@users.noreply.github.com>
2021-01-02 18:01:20 +0100

    Add react slideshow package

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,6 +1,7 @@
 import React from "react";
 import { ToastContainer } from "react-toastify";
 import "react-toastify/dist/ReactToastify.css";
+import "react-slideshow-image/dist/styles.css";
 import Routes from "./routes";
 import "./style.css";
 

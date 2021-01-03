58ca1bc1891a6c37eede443593e0a0a53b1cf683
HannaGregorowicz <33255939+HannaGregorowicz@users.noreply.github.com>
2021-01-03 13:34:58 +0100

    Remove unused code

--- frontend/src/components/pages/instructors/Slideshow.tsx
+++ frontend/src/components/pages/instructors/Slideshow.tsx
@@ -3,13 +3,6 @@ import { Slide } from "react-slideshow-image";
 import { IInstructor } from "../../../data/dataTypes";
 import Instructor from "./Instructor";
 
-const slideImages = [
-  "https://images.unsplash.com/photo-1509721434272-b79147e0e708?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1500&q=80",
-  "https://images.unsplash.com/photo-1506710507565-203b9f24669b?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1536&q=80",
-  "https://images.unsplash.com/photo-1536987333706-fc9adfb10d91?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1500&q=80",
-  "https://images.unsplash.com/photo-1444525873963-75d329ef9e1b?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1500&q=80"
-];
-
 const containerStyle: React.CSSProperties = {
   width: "80%",
   margin: "30px auto"
@@ -28,15 +21,7 @@ const Slideshow = (props: IProps) => {
 
   return (
     <div style={containerStyle}>
-      <Slide easing="ease">
-        {/* <div className="each-slide"> */}
-        {/* <div style={{ backgroundImage: `url(${slideImages[0]})` }}>
-            <span>Slide 1</span>
-          </div> */}
-        {/* <img src={slideImages[0]} alt="img" /> */}
-
-        {instructors.map(makeInstructor)}
-      </Slide>
+      <Slide easing="ease">{instructors.map(makeInstructor)}</Slide>
     </div>
   );
 };
